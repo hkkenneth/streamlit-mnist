0a92bc876446490d4f933b8edaa806bf95cf3d99
Kenneth Lui <hkkenneth@users.noreply.github.com>
2022-02-20 03:27:16 +0800

    Try column layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,38 +21,40 @@ if os.path.exists('mnist-8.onnx'):
 
 test_images = mnist.test_images()
 
-chosen_index = st.number_input('Choose an image', min_value=1, max_value=10000, value=1, step=1)
+col1, col2 = st.columns(2)
 
-if st.button('Predict'):
+chosen_index = col1.number_input('Choose an image', min_value=1, max_value=10000, value=1, step=1)
+
+if col1.button('Predict'):
   output = session.run([], {input_name: [[test_images[int(chosen_index) - 1].astype(np.float32)]]})[0]
   #print(np.argmax([o[0] for o in outputs], axis=1))
-  st.write(np.argmax(output))
+  col1.write(np.argmax(output))
 
 plt.imshow(test_images[int(chosen_index) - 1], interpolation='nearest')
-st.pyplot(plt)
+col1.pyplot(plt)
 
 # Ref: https://docs.streamlit.io/library/api-reference/widgets/st.button
-if st.button('Download chosen image'):
+if col1.button('Download chosen image'):
   # Ref: https://gist.github.com/xkumiyu/c93222f2dce615f4b264a9e71f6d49e0
   Image.fromarray(test_images[int(chosen_index) - 1].reshape(28, 28)).save('user-download.png')
-  st.write('Image ready for download')
+  col1.write('Image ready for download')
   with open('user-download.png', "rb") as file:
     # Ref: https://docs.streamlit.io/library/api-reference/widgets/st.download_button
-    btn = st.download_button(label="Download",
-                             data=file,
-                             file_name="mnist_test_%i.png" % (int(chosen_index) - 1),
-                             mime="image/png"
-                            )
+    btn = col1.download_button(label="Download",
+                               data=file,
+                               file_name="mnist_test_%i.png" % (int(chosen_index) - 1),
+                               mime="image/png"
+                              )
 
-upload_file = st.file_uploader(label='Predict uploaded MNIST image file')
+upload_file = col2.file_uploader(label='Predict uploaded MNIST image file')
 
 if upload_file is not None:
   # To read file as bytes:
   bytes_data = upload_file.getvalue()
   uploaded_image = Image.open(io.BytesIO(bytes_data))
   # Ref: https://docs.streamlit.io/library/api-reference/media/st.image
-  st.image(uploaded_image, 'Uploaded image')
+  col2.image(uploaded_image, 'Uploaded image')
   output = session.run([], {input_name: [[ np.asarray(uploaded_image).reshape(28, 28).astype(np.float32)]]})[0]
-  st.write('Predicted output: %d' % np.argmax(output))
-  st.write('Raw model output:')
-  st.table(output.transpose())
+  col2.write('Predicted output: %d' % np.argmax(output))
+  col2.write('Raw model output:')
+  col2.table(output.transpose())
