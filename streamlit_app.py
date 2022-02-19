90522423e121b33276429c1de0288fa718620156
Kenneth Lui <hkkenneth@users.noreply.github.com>
2022-02-20 03:07:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ import onnxruntime
 import mnist
 import numpy as np
 import urllib.request
+from PIL import Image
 from matplotlib import pyplot as plt
+
 
 onnx_model_url = 'https://github.com/onnx/models/blob/main/vision/classification/mnist/model/mnist-8.onnx?raw=true'
 if not os.path.exists('mnist-8.onnx'):
@@ -19,17 +21,30 @@ if os.path.exists('mnist-8.onnx'):
 test_images = mnist.test_images()
 
 chosen_index = st.number_input('Choose an image', min_value=1, max_value=10000, value=1, step=1)
+
+if st.button('Predict'):
+  output = session.run([], {input_name: [[test_images[int(chosen_index) - 1].astype(np.float32)]]})[0]
+  #print(np.argmax([o[0] for o in outputs], axis=1))
+  st.write(np.argmax(output))
+
 plt.imshow(test_images[int(chosen_index) - 1], interpolation='nearest')
 st.pyplot(plt)
-  
+
+if st.button('Download chosen image'):
+  Image.fromarray(test_images[int(chosen_index) - 1].reshape(28, 28)).save('user-download.png')
+  st.write('Image ready for download)'
+  with open('user-download.png', "rb") as file:
+    btn = st.download_button(label="Download",
+                             data=file,
+                             file_name="mnist_test_%i.png" % (int(chosen_index) - 1),
+                             mime="image/png"
+                            )
+
 upload_file = st.file_uploader(label='Upload Image File (TODO)')
 
 if upload_file is not None:
   # To read file as bytes:
   bytes_data = upload_file.getvalue()
-  st.write(bytes_data)
-
-if st.button('Predict'):
-  output = session.run([], {input_name: [[test_images[int(chosen_index) - 1].astype(np.float32)]]})[0]
+  #output = session.run([], {input_name: [[ ...astype(np.float32)]]})[0]
   #print(np.argmax([o[0] for o in outputs], axis=1))
-  st.write(np.argmax(output) + 1)
+  #st.write(np.argmax(output) + 1)
